174fb5d348d76e78d46c29e9f61ea27bec14dbee
Nima6666 <lamap8708@gmail.com>
2023-09-15 03:52:17 +0545

    all

---
+++ dist/todob870b899dc8c46640fa7.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+console.log('akshjdgkajshdgl')
+/******/ })()
+;
+//# sourceMappingURL=todob870b899dc8c46640fa7.js.map
